452685d5c7e3f4831c516c3c7e2d9fa2d46851d0
guoyifan3 <guoyifan1@jd.com>
2018-10-15 17:00:39 +0800

    first commit

---
+++ precache-manifest.d5a9e35b392c43f4c7639fb1c27fa8a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0d8b9a96ad7cbb991be7",
+    "url": "/static/js/main.0d8b9a96.chunk.js"
+  },
+  {
+    "revision": "a813c7260cea9763c3aa",
+    "url": "/static/js/1.a813c726.chunk.js"
+  },
+  {
+    "revision": "0d8b9a96ad7cbb991be7",
+    "url": "/static/css/main.a80caff4.chunk.css"
+  },
+  {
+    "revision": "a813c7260cea9763c3aa",
+    "url": "/static/css/1.820af673.chunk.css"
+  },
+  {
+    "revision": "392085c5247791e954fa3afe8abcb052",
+    "url": "/index.html"
+  }
+];
